eaf01385a3a32cb45f118e64da2457e9f3458348
kvnblr <kevinbalauro@hotmail.com>
2026-08-11 10:04:42 +0800

    add log configuration to program

--- Management.Api/Program.cs
+++ Management.Api/Program.cs
@@ -1,9 +1,13 @@
 using Management.Api.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 builder.Services.ConfigureCors();
+builder.Services.ConfigureLogging();
 
 var app = builder.Build();
 
