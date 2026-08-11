86f41049c3fae35df358222aa8a84a1b22df7ab4
kvnblr <kevinbalauro@hotmail.com>
2026-08-11 09:02:26 +0800

    add configuration to program

--- Management.Api/Program.cs
+++ Management.Api/Program.cs
@@ -1,8 +1,16 @@
+using Management.Api.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureCors();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All });
+app.UseCors("CorsPolicy");
 
 app.Run();
 
