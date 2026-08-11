73db3b6abc40e933b5ba84a11c50bd532544bb48
kvnblr <kevinbalauro@hotmail.com>
2026-08-11 09:02:02 +0800

    add cors configuration

---
+++ Management.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+namespace Management.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()));
+}
