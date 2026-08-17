5b7bcbbc94856008966c6fe1ee9cddcb33b83a37
kvnblr <kevinbalauro@hotmail.com>
2026-08-17 22:17:15 +0800

    create base repository interface

---
+++ Management.Contracts/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+using System.Linq.Expressions;
+
+namespace Management.Contracts;
+
+public interface IRepositoryBase<T>
+{
+    IQueryable<T> FindAll(bool trackChanges);
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+    void Create(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+}
