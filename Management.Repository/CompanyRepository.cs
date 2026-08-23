4dc95e0e2f135b77553e2011a34209afaf1589e2
kvnblr <kevinbalauro@hotmail.com>
2026-08-23 13:16:59 +0800

    implement get all companies method

--- Management.Repository/CompanyRepository.cs
+++ Management.Repository/CompanyRepository.cs
@@ -6,5 +6,6 @@ namespace Management.Repository;
 public class CompanyRepository(RepositoryContext repositoryContext)
     : RepositoryBase<Company>(repositoryContext), ICompanyRepository
 {
-
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges).OrderBy(c => c.Name).ToList();
 }
