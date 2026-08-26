e0ef60d4ef164a8123b30f4c3e6c53b740754439
kvnblr <kevinbalauro@hotmail.com>
2026-08-26 21:03:18 +0800

    add automapper configuration method

--- Management.Api/Extensions/ServiceExtensions.cs
+++ Management.Api/Extensions/ServiceExtensions.cs
@@ -1,8 +1,10 @@
 using Management.Contracts;
+using Management.Entities.Models;
 using Management.Repository;
 using Management.Service;
 using Management.Service.Contracts;
 using Management.Service.Log;
+using Management.Shared.DataTransferObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace Management.Api.Extensions;
@@ -27,4 +29,12 @@ public static class ServiceExtensions
 
     public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration) =>
         services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+    public static void ConfigureAutoMapping(this IServiceCollection services) =>
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.CreateMap<Company, CompanyDto>()
+            .ForCtorParam("FullAddress", opt => opt.MapFrom(x => x.Address + " " + x.Country));
+            // .ForMember(c => c.FullAddress, opt => opt.MapFrom(x => x.Address + " " + x.Country));
+        }, typeof(Program));
 }
