e99c7e38b1a9317f8ca1a39df5323a612484fa2c
kvnblr <kevinbalauro@hotmail.com>
2026-08-28 12:39:09 +0800

    implement get company method

--- Management.Repository/CompanyRepository.cs
+++ Management.Repository/CompanyRepository.cs
@@ -8,4 +8,7 @@ public class CompanyRepository(RepositoryContext repositoryContext)
 {
     public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
         FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+
+    public Company GetCompany(Guid id, bool trackChanges) =>
+        FindByCondition(c => c.Id.Equals(id), trackChanges).SingleOrDefault();
 }
