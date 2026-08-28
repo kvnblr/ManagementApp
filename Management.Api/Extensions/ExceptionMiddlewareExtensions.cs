b42d49c495afd61445918a6aa7d1db5cafe2a2ce
kvnblr <kevinbalauro@hotmail.com>
2026-08-28 11:40:10 +0800

    create custom error handler

---
+++ Management.Api/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using Management.Contracts;
+using Management.Entities.ErrorDetails;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace Management.Api.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void ConfigureExceptionHandler(this WebApplication app, ILoggerManager logger)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "applicaton/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature != null)
+                {
+                    logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                    await context.Response.WriteAsync(new ErrorDetails()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = "InternalServerError"
+                    }.ToString());
+                }
+
+            });
+        });
+    }
+}
