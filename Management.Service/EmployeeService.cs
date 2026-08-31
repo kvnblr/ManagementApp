40593f429ea6193ecaa2d6e08a98e28dc0915d13
kvnblr <kevinbalauro@hotmail.com>
2026-08-31 20:28:02 +0800

    implement get employees method

--- Management.Service/EmployeeService.cs
+++ Management.Service/EmployeeService.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Management.Contracts;
+using Management.Entities.Exceptions;
 using Management.Service.Contracts;
+using Management.Shared.DataTransferObjects;
 
 namespace Management.Service;
 
@@ -10,5 +12,11 @@ internal sealed class EmployeeService(
         IMapper mapper)
     : IEmployeeService
 {
-
+    public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+    {
+        var company = repositoryManager.Company.GetCompany(companyId, trackChanges) ?? throw new CompanyNotFoundException(companyId);
+        var employees = repositoryManager.Employee.GetEmployees(companyId, trackChanges);
+        var employeesDto = mapper.Map<IEnumerable<EmployeeDto>>(employees);
+        return employeesDto;
+    }
 }
