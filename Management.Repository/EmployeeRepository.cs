85a0c236adfd869c1067dcfd9db8677bd35bf59a
kvnblr <kevinbalauro@hotmail.com>
2026-08-31 20:38:34 +0800

    implement get employee method

--- Management.Repository/EmployeeRepository.cs
+++ Management.Repository/EmployeeRepository.cs
@@ -6,6 +6,9 @@ namespace Management.Repository;
 public class EmployeeRepository(RepositoryContext repositoryContext)
     : RepositoryBase<Employee>(repositoryContext), IEmployeeRepository
 {
+    public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
+        FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges).SingleOrDefault()!;
+
     public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
         FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .OrderBy(e => e.Name).ToList();
