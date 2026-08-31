7169e7a05650337b4d586a198f3a7f464e96c2cb
kvnblr <kevinbalauro@hotmail.com>
2026-08-31 20:30:10 +0800

    add get employees for company method

--- Management.Presentation/EmployeesController.cs
+++ Management.Presentation/EmployeesController.cs
@@ -7,5 +7,11 @@ namespace Management.Presentation;
 [Route("/api/companies/{companyId}/[controller]")]
 public class EmployeesController(IServiceManager serviceManager) : ControllerBase
 {
+    [HttpGet]
+    public IActionResult GetEmployeesForCompany(Guid companyId)
+    {
+        var employees = serviceManager.Employee.GetEmployees(companyId, trackChanges: false);
+        return Ok(employees);
+    }
 
 }
