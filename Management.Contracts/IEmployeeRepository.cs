03186ef847179c28ff3da9760bcdd24a1c55de24
kvnblr <kevinbalauro@hotmail.com>
2026-08-31 19:05:39 +0800

    create get employees method

--- Management.Contracts/IEmployeeRepository.cs
+++ Management.Contracts/IEmployeeRepository.cs
@@ -1,4 +1,8 @@
+using Management.Entities.Models;
+
 namespace Management.Contracts;
 
 public interface IEmployeeRepository
-{ }
+{
+    IEnunmerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
+}
