0229a679b6b7fa916b992d4df2e89b37e2713aec
kvnblr <kevinbalauro@hotmail.com>
2026-09-01 10:24:01 +0800

    configure to add a list of employee when creating

--- Management.Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ Management.Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -1,3 +1,3 @@
 namespace Management.Shared.DataTransferObjects;
 
-public record CompanyForCreationDto(string Name, string Address, string Country);
+public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
