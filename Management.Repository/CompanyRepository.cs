48221fbc9b91353a5a93698ef961c8ed73364aa6
kvnblr <kevinbalauro@hotmail.com>
2026-09-01 09:21:50 +0800

    implement create company method

--- Management.Repository/CompanyRepository.cs
+++ Management.Repository/CompanyRepository.cs
@@ -6,6 +6,8 @@ namespace Management.Repository;
 public class CompanyRepository(RepositoryContext repositoryContext)
     : RepositoryBase<Company>(repositoryContext), ICompanyRepository
 {
+    public void CreateCompany(Company company) => Create(company);
+
     public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
         FindAll(trackChanges).OrderBy(c => c.Name).ToList();
 
