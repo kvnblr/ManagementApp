117cca0ed1e19c0b4f4eb5c76ebb8783b03ed32c
kvnblr <kevinbalauro@hotmail.com>
2026-09-05 20:52:53 +0800

    add get by ids method

--- Management.Contracts/ICompanyRepository.cs
+++ Management.Contracts/ICompanyRepository.cs
@@ -5,6 +5,8 @@ namespace Management.Contracts;
 public interface ICompanyRepository
 {
     IEnumerable<Company> GetAllCompanies(bool trackChanges);
+    IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
     Company GetCompany(Guid id, bool trackChanges);
     void CreateCompany(Company company);
+
 }
