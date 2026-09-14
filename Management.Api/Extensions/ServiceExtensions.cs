dae83cb4f084d73d0ba92cef99c5c22ca8e14bb0
kvnblr <kevinbalauro@hotmail.com>
2026-09-14 13:50:29 +0800

    add a reverse map

--- Management.Api/Extensions/ServiceExtensions.cs
+++ Management.Api/Extensions/ServiceExtensions.cs
@@ -58,7 +58,7 @@ public static class ServiceExtensions
 
             cfg.CreateMap<Employee, EmployeeDto>();
             cfg.CreateMap<EmployeeForCreationDto, Employee>();
-            cfg.CreateMap<EmployeeForUpdateDto, Employee>();
+            cfg.CreateMap<EmployeeForUpdateDto, Employee>().ReverseMap();
         }, typeof(Program));
 
     private static NewtonsoftJsonInputFormatter GetJsonPatchInputFormatter() =>
