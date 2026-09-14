6108b9728b9fcdd384ed24bfb52e476e776ea7cb
kvnblr <kevinbalauro@hotmail.com>
2026-09-14 13:00:57 +0800

    add validation

--- Management.Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Management.Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -1,3 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Management.Shared.DataTransferObjects;
 
-public record EmployeeForCreationDto(string Name, int Age, string Position);
+public record EmployeeForCreationDto
+{
+    [Required(ErrorMessage = "Employee name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+    public string? Name { get; init; }
+
+    [Required(ErrorMessage = "Age is a required field.")]
+    int Age { get; init; }
+
+    [Required(ErrorMessage = "Eployee position is a required field.")]
+    [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+    public string? Position { get; init; }
+}
