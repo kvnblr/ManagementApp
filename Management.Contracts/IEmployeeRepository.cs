f7a3a3dcd1f9de32ffcd98f22e2c1b9b2d9ed4f0
kvnblr <kevinbalauro@hotmail.com>
2026-09-16 16:07:17 +0800

    add get employees with parameters method

--- Management.Contracts/IEmployeeRepository.cs
+++ Management.Contracts/IEmployeeRepository.cs
@@ -8,6 +8,7 @@ public interface IEmployeeRepository
     Employee GetEmployee(Guid companyId, Guid id, bool trackChanges);
 
     Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges);
+    Task<IEnumerable<Employee>> GetEmployeesWithParametersAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges);
     Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
     void CreateEmployeeForCompany(Guid companyId, Employee employee);
     void DeleteEmployee(Employee employee);
