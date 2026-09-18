b1ef1d3dd9b555b3beebe1a4bd81bc31ba303afd
kvnblr <kevinbalauro@hotmail.com>
2026-09-18 14:49:20 +0800

    add filter, search extension method

---
+++ Management.Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -0,0 +1,16 @@
+using Management.Entities.Models;
+
+namespace Management.Repository.Extensions;
+
+public static class RepositoryEmployeeExtensions
+{
+    public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge) =>
+        employees.Where(e => e.Age >= minAge && e.Age <= maxAge);
+
+    public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string searchTerm)
+    {
+        if (string.IsNullOrWhiteSpace(searchTerm)) return employees;
+        var lowerCaseTerm = searchTerm.Trim().ToLower();
+        return employees.Where(e => e.Name!.Contains(lowerCaseTerm, StringComparison.CurrentCultureIgnoreCase));
+    }
+}
